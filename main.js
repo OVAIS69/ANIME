5400c26708a70f27533ae310d5e167e4a940c762
OVAIS69 <147296875+OVAIS69@users.noreply.github.com>
2024-07-12 17:24:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if( $(window).scrollTop() > 70) {
+      $('.container').addClass('nav-fixed');
+    }else {
+      $('.container').removeClass('nav-fixed');
+    }
+  });
+
+  $('.list-wrap').jScrollPane();
+
+  var i = 1;
+  setInterval(function() {
+    if (i == 4) {
+      $('.image4').removeClass('current');
+      $('.image1').addClass('current');
+      i = 1;
+    }else {
+      $('.image'+i).removeClass('current');
+      i++;
+      $('.image'+i).addClass('current');
+    }
+  },8000);
+});
